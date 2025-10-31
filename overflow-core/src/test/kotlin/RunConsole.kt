bd3ca2949398607251aba540c3516d339446e6b9
MrXiaoM <coolxiaom95@gmail.com>
2025-11-01 02:02:38 +0800

    revert `Update RunConsole.kt`

--- overflow-core/src/test/kotlin/RunConsole.kt
+++ overflow-core/src/test/kotlin/RunConsole.kt
@@ -3,18 +3,9 @@ import net.mamoe.mirai.console.MiraiConsole
 import net.mamoe.mirai.console.terminal.MiraiConsoleImplementationTerminal
 import net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader
 import net.mamoe.mirai.console.util.ConsoleExperimentalApi
-import java.net.URI
 
 @OptIn(ConsoleExperimentalApi::class, ConsoleFrontEndImplementation::class)
 suspend fun main() {
-    val uri = URI("http://127.0.0.1:8080/api?access_token=awa")
-    println(uri.resolve(buildString {
-        append(uri.path.removeSuffix("/"))
-        append("/")
-        append("send_message")
-        if (uri.query.isNotEmpty()) {
-            append("?")
-            append(uri.query)
-        }
-    }))
+    MiraiConsoleTerminalLoader.startAsDaemon(MiraiConsoleImplementationTerminal())
+    MiraiConsole.job.join()
 }
